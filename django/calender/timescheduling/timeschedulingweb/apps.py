f3a9d333291040997662ec095a820f7f7575c3f6
bhaktiandhare1511 <riddhi.pankhade@gmail.com>
2025-03-18 16:16:52 +0530

    changes

--- django/calender/timescheduling/timeschedulingweb/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TimeschedulingwebConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'timeschedulingweb'
--- django/calender/timescheduling/timeschedulingweb/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Timeslot(models.Model):
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    discription = models.TextField()
